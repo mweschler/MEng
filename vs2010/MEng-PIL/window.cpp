779d9b95be7a3b5d0911d115e7625c7400ac317f
Michael Weschler <mweschler@gmail.com>
2013-07-07 17:56:26 -0400

    window start

--- vs2010/MEng-PIL/window.cpp
+++ vs2010/MEng-PIL/window.cpp
@@ -6,6 +6,7 @@ namespace MEng{
 	Window::Window()
 		:m_width(800)
 		,m_height(600)
+		,m_fullscreen(false)
 	{
 	
 	}
@@ -18,5 +19,32 @@ namespace MEng{
 		ASSERT((width > 0) && (height > 0));
 		this->m_width = width;
 		this->m_height = height;
+	}
+
+	void Window::setFullscreen(bool fullscreen){
+		this->m_fullscreen = fullscreen;
+	}
+
+	bool Window::create(){
+#ifdef _WIN32
+		m_hinstance = GetModuleHandle(NULL);
+		m_appname = L"MEng";
+
+		WNDCLASSEX winclass;
+		winclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+		winclass.lpfnWndProc = WndProc;
+		winclass.cbClsExtra = 0;
+		winclass.cbWndExtra = 0;
+		winclass.hInstance = this->m_hinstance;
+		winclass.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+		winclass.hIconSm = winclass.hIcon;
+		winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+		winclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+		winclass.lpszMenuName = NULL;
+		winclass.lpszClassName = this->m_appname;
+		winclass.cbSize = sizeof(WNDCLASSEX);
+		return true;
+#endif
+		
 	}
 }
--- vs2010/MEng-PIL/window.h
+++ vs2010/MEng-PIL/window.h
@@ -1,14 +1,30 @@
 #pragma once
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#ifdef _WIN32
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+#endif
+
 namespace MEng{
 	class Window{
 	public:
 		Window();
 		~Window();
 		void setDimensions(int width, int height);
+		void setFullscreen(bool fullscreen);
+		bool create();
 	private:
 		int m_width;
 		int m_height;
+		bool m_fullscreen;
+		#ifdef _WIN32
+		HWND m_handle;
+		HINSTANCE m_hinstance;
+		LPCWSTR m_appname;
+		#endif
 	};
 
 }
